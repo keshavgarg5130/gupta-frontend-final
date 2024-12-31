881d986c9354fe3ae6815bfea6396b7ea7568176
JATIN-RAGHAV <jrag137@gmail.com>
2024-12-31 14:54:28 +0530

    half made with options

--- src/app/ui/CategoryPage.tsx
+++ src/app/ui/CategoryPage.tsx
@@ -1,13 +1,31 @@
 import fetchProducts from "../lib/fetchProducts";
 import fetchCategories from "../lib/fetchCategories";
 import CategoryPageClient from './CategoryPageClient'
+import poles from "../interfaces/poles";
+import currentRatingInterface from "../interfaces/currentRating";
 
 export const revalidate = 3600;
 
 export default async function CategoryPage({ category }: { category: string }) {
   const response = await fetchProducts();
   const products = response.filter(prdt => prdt.category.name === category)
+  let poles: poles[] = [];
+  let polesIds: string[] = [];
+  let currentRatings: currentRatingInterface[] = [];
+  let currentRatingsIds: string[] = [];
+
+  for (let prdt of products) {
+    if (polesIds.indexOf(prdt.polesId) == -1) {
+      polesIds.push(prdt.polesId)
+      poles.push(prdt.poles)
+    }
+    if (currentRatingsIds.indexOf(prdt.currentRatingId) == -1) {
+      currentRatingsIds.push(prdt.currentRatingId)
+      currentRatings.push(prdt.currentRating)
+    }
+  }
+
   const bannerId = (await fetchCategories()).filter(categor => categor.name == category)[0].billboardId;
 
-  return (<CategoryPageClient products={products} bannerId={bannerId} />)
+  return (<CategoryPageClient products={products} bannerId={bannerId} poles={poles} currentRatings={currentRatings} />)
 }
--- src/app/ui/CategoryPageClient.tsx
+++ src/app/ui/CategoryPageClient.tsx
@@ -3,6 +3,8 @@ import { Dispatch, SetStateAction, useState, useEffect } from "react"
 import { product } from "../interfaces/product"
 import CategoryBanner from "./CategoryBanner"
 import ProductFullRow from "./ProductFullRow"
+import poles from "../interfaces/poles"
+import currentRatingInterface from "../interfaces/currentRating"
 
 const SortBy = ['price', 'currentRating', 'poles', ''] as const;
 type SortByType = typeof SortBy[number];
@@ -10,32 +12,55 @@ type typeSort = {
     name: SortByType,
     increasing: boolean
 }
-type filterType = {
-    price: number,
-    poles: number,
-    currentRating: number
+
+interface filterStateInterface {
+    [id: string | number]: {
+        isSelected: boolean,
+        name: string
+    },
 }
 
-const CategoryPageClient = ({ bannerId, products }: { bannerId: string, products: product[] }) => {
+const CategoryPageClient = ({ bannerId, products, poles, currentRatings }: { bannerId: string, products: product[], poles: poles[], currentRatings: currentRatingInterface[] }) => {
     const [productsSort, setProducts] = useState(products)
+    const [increasing, setIncreasing] = useState({ price: true, poles: true, currentRating: true })
     const [sortBy, setSortBy] = useState<typeSort>({
         name: '',
         increasing: true
     })
     const remains = products.length > 0
-    const [filter, SetFilter] = useState<filterType>({
+    const [filter, setFilter] = useState<{ price: number }>({
         price: 0,
-        poles: 0,
-        currentRating: 0
     })
 
-    useEffect(() => {
-        let prdts = [...products]
-        prdts = prdts.filter(product => parseInt(product.price) >= filter.price)
-        prdts = prdts.filter(product => parseInt(product.poles.name) >= filter.poles)
-        prdts = prdts.filter(product => parseInt(product.currentRating.name) >= filter.currentRating)
-        setProducts(prdts)
+    let polesState: filterStateInterface = {};
+    for (let pole of poles) {
+        polesState[pole.id] = {
+            isSelected: true,
+            name: pole.name
+        }
+    }
+    let currentRatingState: filterStateInterface = {};
+    for (let currentRating of currentRatings) {
+        currentRatingState[currentRating.id] = {
+            isSelected: true,
+            name: currentRating.name
+        }
+    }
 
+    const [polesSelected, setPolesSelected] = useState<filterStateInterface>(polesState)
+    const [currentRatingsSelected, setCurrentRatingsSelected] = useState<filterStateInterface>(currentRatingState)
+
+    useEffect(() => {
+        console.log(polesSelected)
+        let prdts = [...productsSort]
+        prdts = prdts.filter(product => {
+            const priceFits = parseInt(product.price) >= filter.price
+            const poleFits = polesSelected[product.polesId].isSelected;
+            const currentFits = currentRatingsSelected[product.currentRatingId].isSelected
+            return priceFits && poleFits && currentFits
+        })
+
+        setProducts(prdts)
         if (sortBy.name) {
             const increasing = sortBy.increasing
             switch (sortBy.name) {
@@ -58,82 +83,73 @@ const CategoryPageClient = ({ bannerId, products }: { bannerId: string, products
                         setProducts(prdts.sort((a, b) => parseInt(b.currentRating.name) - parseInt(a.currentRating.name)))
             }
         }
-    }, [sortBy, filter])
+    }, [sortBy, filter, polesSelected, currentRatingsSelected])
 
     return (
         <div className="flex flex-col items-center w-full relative pt-16 md:pt-0">
             <CategoryBanner bannerId={bannerId} />
             <ProductFullRow products={productsSort} Remains={remains} />
-            <FilterMenu setSortBy={setSortBy} filter={filter} setFilter={SetFilter} />
+            {/* Filter Menu */}
+            <div className="absolute top-0 left-0 w-full h-14 md:w-[200px] lg:w-[300px] md:h-full bg-blue-100">
+                <div>
+                    <h1> Sort By </h1>
+                    <div className="flex flex-col">
+                        {SortBy.map(sort => {
+                            if (sort) {
+                                return <button key={sort} onClick={() => {
+                                    setSortBy({
+                                        name: sort,
+                                        increasing: increasing[sort]
+                                    })
+                                    setIncreasing(increasing => {
+                                        return {
+                                            ...increasing,
+                                            [sort]: !increasing[sort]
+                                        }
+                                    })
+                                }}>{sort}</button>
+                            }
+                        })}
+                    </div>
+                </div>
+
+                <div>
+                    <div>
+                        <label>Price:</label>
+                        <input name='price' onChange={e => {
+                            if (parseInt(e.target.value)) {
+                                setFilter({
+                                    ...filter,
+                                    [e.target.name]: e.target.value
+                                })
+                            }
+                        }} />
+                    </div>
+                    <div>
+                        {poles.map(pole => {
+                            return (<button onClick={() => {
+                                setPolesSelected({
+                                    ...polesSelected,
+                                    [pole.id]: {
+                                        name: pole.name,
+                                        isSelected: !polesSelected[pole.id].isSelected
+                                    }
+                                })
+                            }}>
+                                <PolesDiv key={pole.id} name={pole.name} isSelected={polesSelected[pole.id].isSelected} />
+                            </button>)
+                        })}
+                    </div>
+                </div>
+            </div >
         </div>
     )
 }
 
-const FilterMenu = ({ setSortBy, setFilter, filter }: { setSortBy: Dispatch<SetStateAction<typeSort>>, setFilter: Dispatch<SetStateAction<filterType>>, filter: filterType }) => {
-
-    const [increasing, setIncreasing] = useState({ price: true, poles: true, currentRating: true })
-
+const PolesDiv = ({ name, isSelected }: { name: string, isSelected: boolean }) => {
+    const classes = `h-[30px] w-max px-4 rounded-full flex items-center justify-center ${isSelected ? 'bg-themeBlue text-white' : 'bg-white text-themeBlue border border-themeBlue'} ${isSelected && 'text-lg font-bold'} hover:bg-themeBlue hover:text-white transition-all duration-300`
     return (
-        <div className="absolute top-0 left-0 w-full h-14 md:w-[200px] lg:w-[300px] md:h-full bg-blue-100">
-            <div>
-                <h1> Sort By </h1>
-                <div className="flex flex-col">
-                    {SortBy.map(sort => {
-                        if (sort) {
-                            return <button key={sort} onClick={() => {
-                                setSortBy({
-                                    name: sort,
-                                    increasing: increasing[sort]
-                                })
-                                setIncreasing(increasing => {
-                                    return {
-                                        ...increasing,
-                                        [sort]: !increasing[sort]
-                                    }
-                                })
-                            }}>{sort}</button>
-                        }
-                    })}
-                </div>
-            </div>
-
-            <div>
-                <div>
-                    <label>Price:</label>
-                    <input name='price' onChange={e => {
-                        if (parseInt(e.target.value)) {
-                            setFilter({
-                                ...filter,
-                                [e.target.name]: e.target.value
-                            })
-                        }
-                    }} />
-                </div>
-                <div>
-                    <label>Current Rating:</label>
-                    <input name='currentRating' onChange={e => {
-                        if (parseInt(e.target.value)) {
-                            setFilter({
-                                ...filter,
-                                [e.target.name]: e.target.value
-                            })
-                        }
-                    }} />
-                </div>
-                <div>
-                    <label>Poles:</label>
-                    <input name='poles' onChange={e => {
-                        if (parseInt(e.target.value)) {
-                            setFilter({
-                                ...filter,
-                                [e.target.name]: e.target.value
-                            })
-                        }
-                    }} />
-                </div>
-            </div>
-        </div >
+        <div className={classes} >{name}</div>
     )
 }
-
 export default CategoryPageClient

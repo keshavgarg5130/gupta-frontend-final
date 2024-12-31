aebaeb72548a1ce980e1af66a04c404e8d50093b
JATIN-RAGHAV <jrag137@gmail.com>
2024-12-31 18:45:51 +0530

    improved Category Page

--- src/app/ui/CategoryPamphlet.tsx
+++ src/app/ui/CategoryPamphlet.tsx
@@ -4,8 +4,12 @@ export const revalidate = 3600;
 
 const CategoryPamphlet = async ({ categoryName, billboardId }: { categoryName: string, billboardId: string }) => {
     const billboardUrl = await fetchBillboard(billboardId)
+    if (categoryName == 'MCB') categoryName = 'Mineature Circuit Breaker (MCB)'
+    else if (categoryName == 'ACB') categoryName = 'Air Circuit Breaker (ACB)'
+    else if (categoryName == 'MCCB') categoryName = 'Molded Case Circuit Breaker (MCCB)'
+    else if (categoryName == 'RCCB') categoryName = 'Residual Current Circuit Breaker (RCCB)'
 
-    return (<a href={'/' + categoryName} className="flex flex-col items-center justify-center gap-1 w-3/4 md:w-1/3 lg:w-1/4 overflow-hidden md:border rounded-lg md:p-10">
+    return (<a href={'/' + categoryName} className="flex flex-col items-center justify-center gap-1 w-3/4 md:w-1/3 lg:w-2/5 overflow-hidden md:border rounded-lg md:p-10">
         <img src={billboardUrl} className="max-w-full max-h-3/4" />
         <h1 className="text-2xl font-bold text-center text-themeBlue relative">{categoryName}
             <div className="w-full -bottom-7 md:h-[2px] bg-themeBlue rounded-lg"></div>

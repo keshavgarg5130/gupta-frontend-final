66a101f40173c585af30ce29d129519f60e6da5d
JATIN-RAGHAV <jrag137@gmail.com>
2025-01-01 17:52:31 +0530

    panels data

---
+++ src/lib/PanelsData.ts
@@ -0,0 +1,44 @@
+const PanelsData = [
+    {
+        name: 'AMF Panels',
+        link: '/AMF'
+    },
+    {
+        name: 'APFC Panels',
+        link: '/APFC'
+    },
+    {
+        name: 'Automatic Street Lighting Switching Panels',
+        link: '/AMF'
+    },
+    {
+        name: 'Busduct / Rising mains',
+        link: '/BusductRising'
+    },
+    {
+        name: 'DG Synchronization Panels',
+        link: '/DGSynchronization'
+    },
+    {
+        name: 'Distribution Board Panels',
+        link: '/DistributionBoard'
+    },
+    {
+        name: 'Load Sharing',
+        link: '/LoadSharing'
+    },
+    {
+        name: 'Ourdoor Feeder Pillars',
+        link: '/OurDoorFeeder'
+    },
+    {
+        name: 'PLC Panel / SCADA System',
+        link: '/PLC_SCADA'
+    },
+    {
+        name: 'Soft Starter & VFC Panel',
+        link: '/Soft-StarterVFD'
+    },
+]
+
+export default PanelsData

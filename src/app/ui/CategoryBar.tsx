93ba7a2c92da8be8a9ad9363c87071923ea6e9fc
JATIN-RAGHAV <jrag137@gmail.com>
2025-01-01 08:54:53 +0530

    Corrected the Category Bar

--- src/app/ui/CategoryBar.tsx
+++ src/app/ui/CategoryBar.tsx
@@ -1,36 +1,46 @@
 'use client'
 import categories from "@/lib/categoryData"
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const CategoryBar = () => {
   const [clicked, setClicked] = useState(false)
   const handleClick = () => {
-    setClicked(!clicked)
+    setTimeout(() => {
+      setClicked(!clicked)
+    }, 10)
   }
+
+  useEffect(() => {
+    addEventListener('click', handleBodyClick)
+    return () => {
+      removeEventListener('click', handleBodyClick)
+    }
+  })
+
+  const handleBodyClick = () => {
+    if (clicked)
+      setClicked(false)
+  }
+
   return (
-    <div className="h-[45px] w-full bg-white flex items-center box-border relative z-40 justify-evenly border-b-2 ">
-      <div className="flex w-full sm:hidden relative" onClick={handleClick}>
+    <div className="h-[45px] w-full bg-white flex items-center box-border relative z-40 justify-evenly border-b-2 md:px-20">
+      <div className="flex w-full relative cursor-pointer" onClick={handleClick}>
         <div className="flex flex-col gap-[3px] mx-3 justify-center mr-2">
           <Bar />
           <Bar />
           <Bar />
         </div>
-        <div>Categories</div>
-        <div className={`absolute top-[34px] w-full overflow-hidden bg-white ${clicked ? '' : 'h-0'}`}>
+        <div className="select-none font-bold">Categories</div>
+        <div className={`absolute top-[34px] md:max-w-[400px] w-full overflow-hidden bg-white transition-all ${clicked ? '' : 'hidden'} shadow-gray-300 shadow-lg rounded-xl flex flex-col gap-1 py-5`}>
           {categories.map((category, index) => {
             return (
-              <div key={index} className="justify-center py-2 border flex transition-[h]">
+              <div key={index} className="justify-center min-h-8 flex">
                 <CategoryButton  {...category} />
               </div>
             )
           })}
         </div>
-      </div>
-      <div className="hidden sm:flex h-[45px] w-full bg-white items-center box-border justify-evenly flex-grow">
-        {categories.map((category, index) => {
-          return <CategoryButton key={index} {...category} />
-        })}
       </div>
     </div>
   )
@@ -38,7 +48,7 @@ const CategoryBar = () => {
 
 const CategoryButton = ({ name, link, }: { name: string, link: string }) => {
   return (
-    <a href={link} className="text-xs font-semibold hover:text-sm hover:shadow-lg transition-all w-full h-full flex justify-center items-center">
+    <a href={link} className="text-xs hover:text-sm transition-all flex justify-center items-center text-gray-600 w-4/5 border-gray-400 border-[0.5px] min-h-full rounded-xl">
       {name}
     </a>
   )

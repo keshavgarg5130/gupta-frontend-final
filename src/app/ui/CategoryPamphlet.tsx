1f0f8f7a4acb5547fa3329f2ee8d33ed63ae5f85
JATIN-RAGHAV <jrag137@gmail.com>
2025-01-02 09:06:21 +0530

    Category pamphlet improved

--- src/app/ui/CategoryPamphlet.tsx
+++ src/app/ui/CategoryPamphlet.tsx
@@ -17,7 +17,7 @@ const CategoryPamphlet = ({ categoryName, billboards, billboardId }: { categoryN
     else if (categoryName == 'MCCB') categoryName = 'Molded Case Circuit Breaker (MCCB)'
     else if (categoryName == 'RCCB') categoryName = 'Residual Current Circuit Breaker (RCCB)'
 
-    return (<a href={'/' + categoryName} className="flex flex-col bg-white items-center justify-center gap-1 w-2/3 md:w-2/3 lg:w-2/5 overflow-hidden md:border rounded-lg md:p-10">
+    return (<a href={'/' + categoryName} className="flex flex-col bg-white items-center justify-center gap-1 w-2/3 md:w-4/5 lg:w-2/5 overflow-hidden md:border rounded-lg md:p-10">
         <img src={url} className="max-w-full max-h-3/4" />
         <h1 className="text-2xl font-bold text-center text-themeBlue underline">{categoryName}
         </h1>

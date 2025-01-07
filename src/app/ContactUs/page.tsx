61863ebd466f48861a10fdefce45705993a8be4b
keshavgarg5130 <keshav.garg.5130@gmail.com>
2025-01-07 18:53:45 +0530

    gstno

--- src/app/ContactUs/page.tsx
+++ src/app/ContactUs/page.tsx
@@ -23,16 +23,21 @@ const Page = () => {
               <div className="text-3xl font-bold">Gupta Associates</div>
               <div className="flex flex-col gap-6 h-[130px]">
                 <a className="flex gap-2" href="https://maps.app.goo.gl/7r2SxZUTjZfJgPE66" target="_blank">
-                  <MapPin color='#0c77b6' />
+                  <MapPin color='#0c77b6'/>
                   <div>4,5 GT Road Ghaziabad</div>
                 </a>
+
                 <a className="flex gap-2" href="tel: +91 9818091111 (Sales)" target="_blank">
-                  <Phone color='#0c77b6' />
+                  <Phone color='#0c77b6'/>
                   <div>+91 9818091111</div>
+                </a>
+                <a className="flex gap-2" href="https://maps.app.goo.gl/7r2SxZUTjZfJgPE66" target="_blank">
+                  <div>GST NO. - 09AJCPG6279Q2ZF</div>
                 </a>
               </div>
             </div>
-            <div className="flex flex-col justify-center gap-6 border-[1px] rounded-xl p-5 shadow-lg shadow-gray-500 flex-1 items-center py-10">
+            <div
+                className="flex flex-col justify-center gap-6 border-[1px] rounded-xl p-5 shadow-lg shadow-gray-500 flex-1 items-center py-10">
               <div className="text-3xl font-bold">Contact Us</div>
               <div className="flex flex-col gap-6 h-[130px]">
                 <a className="flex gap-2" href="tel: +91 9999706609 (Sales)" target="_blank">

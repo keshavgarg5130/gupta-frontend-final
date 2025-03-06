641ea63ad7c1752ca8588ba4717fc9a97aa5e0f6
keshavgarg5130 <keshav.garg.5130@gmail.com>
2025-03-07 02:13:34 +0530

    Image Optimization

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*).(png|jpg|jpeg|webp|svg|gif)", // Match image files
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable", // Cache for 1 year
+          },
+          {
+            key: "Expires",
+            value: new Date(Date.now() + 31536000000).toUTCString(), // 1 year from now
+          },
+        ],
+      },
+    ];
+  },
   images: {
     domains: ['res.cloudinary.com'], // Add the domain here
   },
